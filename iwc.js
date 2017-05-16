fc03ce84841f341347f0dbaa05318ba0e4a58e9b
jonas k <jonas.koenning@gmail.com>
2017-05-16 11:37:29 +0200

    Wrapping the lib

--- iwc.js
+++ iwc.js
@@ -30,109 +30,124 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 // WIP re-implementation
-iwc = function(){
-};
 
-iwc.Client = function(categories) {
-
-	this._componentName = "unknown";
+(function(window){
 	
-	if (typeof window.location !== "undefined" &&
-			typeof window.location.search === "string" &&
-			typeof window.unescape === "function") {
-		var pairs = window.location.search.substring(1).split("&"), pair, query = {};
-		if (!(pairs.length == 1 && pairs[0] === "")) {
-			for (var p = 0; p < pairs.length; p++) {
-				pair = pairs[p].split("=");
-				if (pair.length == 2) {
-					query[pair[0]] =
-						window.unescape(pair[1]);
+	'use strict';
+	function define_IWC(){
+		var IWC = {};
+		iwc.Client = function(categories) {
+
+		this._componentName = "unknown";
+		
+		if (typeof window.location !== "undefined" &&
+				typeof window.location.search === "string" &&
+				typeof window.unescape === "function") {
+			var pairs = window.location.search.substring(1).split("&"), pair, query = {};
+			if (!(pairs.length == 1 && pairs[0] === "")) {
+				for (var p = 0; p < pairs.length; p++) {
+					pair = pairs[p].split("=");
+					if (pair.length == 2) {
+						query[pair[0]] =
+							window.unescape(pair[1]);
+					}
 				}
 			}
+			if (typeof query.url === "string") {
+				this._componentName = query.url;
+			}
+			//console.log("COMPONENT NAME " + this._componentName);
 		}
-		if (typeof query.url === "string") {
-			this._componentName = query.url;
+		
+		//private variables
+		this._connected = false;
+		this._categories = categories;
+		this._callback = null;
+		
+		//onIntent is called when an intent is received. A JSON intent object is passed to the function
+		this.onIntent = function(){};
+	};
+
+	/**
+	 * Connect widget to messaging. This sets up the callback function and creates
+	 * an event listener for HTML5 messaging.
+	 * @param {function} callback - The callback function used for receiving messages.
+	 */
+	iwc.Client.prototype.connect = function(callback) {
+		this._callback = callback;
+		window.addEventListener('message',callback,false);
+	};
+
+	/**
+	 * Disconnect the widget from messaging. This removes the event listener
+	 * and the callback.
+	 */
+	iwc.Client.prototype.disconnect = function() {
+		window.removeEventListener('message', this._callback, false);
+		this._callback = null;
+	};
+
+	// first send locally, if flag iwc.util.FLAGS is set send via yjs
+	/**
+	 * Publishes an intent, 
+	 * @param {intent} - The intent about to be published, this object contains all information.
+	 */
+	iwc.Client.prototype.publish = function(intent) {
+		if (iwc.util.validateIntent(intent)) {
+			//var envelope = {"type":"JSON", "event":"publish", "message":intent};
+			publishLocal(intent);		
 		}
-		//console.log("COMPONENT NAME " + this._componentName);
+	};
+
+	var publishLocal = function(intent) {
+		//Find iframe and post message
+		//alert("Sender: " + intent.sender);
+		var frames = $(".widget",parent.document).find("iframe");
+		frames.each(function(){
+			if ($(this).contents().find("head").find("title").text() === intent.receiver) {
+				//alert("Posting message");
+				this.contentWindow.postMessage(intent, "http://127.0.0.1:8073");
+			}			
+		});
+	};
+
+	//======================= iwc.util ==============================
+	iwc.util = function() {
+	};
+
+	/**
+	 * Used to determine whether global or local messaging should be used.
+	 * Local messaging uses HTML5 messaging, global messaging uses yjs.
+	 */
+	iwc.util.FLAGS = {
+		PUBLISH_LOCAL : "PUBLISH_LOCAL",
+		PUBLISH_GLOBAL : "PUBLISH_GLOBAL"
+	};
+
+	/**
+	 * Check intent for correctness.
+	 */
+	iwc.util.validateIntent = function(intent) {
+		if (typeof intent.sender != "string") {
+			throw new Error("Intent object must possess property 'component' of type 'String'");
+		}
+		if (typeof intent.data != "string") {
+			throw new Error("Intent object must possess property 'data' of type 'String'");
+		}
+		if (typeof intent.dataType != "string") {
+			throw new Error("Intent object must possess property 'dataType' of type 'String'");
+		}
+		return true;
+	};
+
+		return IWC;
 	}
-	
-	//private variables
-	this._connected = false;
-	this._categories = categories;
-	this._callback = null;
-	
-	//onIntent is called when an intent is received. A JSON intent object is passed to the function
-	this.onIntent = function(){};
-};
 
-/**
- * Connect widget to messaging. This sets up the callback function and creates
- * an event listener for HTML5 messaging.
- * @param {function} callback - The callback function used for receiving messages.
- */
-iwc.Client.prototype.connect = function(callback) {
-	this._callback = callback;
-	window.addEventListener('message',callback,false);
-};
-
-/**
- * Disconnect the widget from messaging. This removes the event listener
- * and the callback.
- */
-iwc.Client.prototype.disconnect = function() {
-	window.removeEventListener('message', this._callback, false);
-	this._callback = null;
-};
-
-// first send locally, if flag iwc.util.FLAGS is set send via yjs
-/**
- * Publishes an intent, 
- * @param {intent} - The intent about to be published, this object contains all information.
- */
-iwc.Client.prototype.publish = function(intent) {
-	if (iwc.util.validateIntent(intent)) {
-		//var envelope = {"type":"JSON", "event":"publish", "message":intent};
-		publishLocal(intent);		
+	//define globally if it doesn't already exist
+	if(typeof(IWC) === 'undefined') {
+		window.IWC = define_IWC();
 	}
-};
-
-var publishLocal = function(intent) {
-	//Find iframe and post message
-	//alert("Sender: " + intent.sender);
-	var frames = $(".widget",parent.document).find("iframe");
-	frames.each(function(){
-		if ($(this).contents().find("head").find("title").text() === intent.receiver) {
-			//alert("Posting message");
-			this.contentWindow.postMessage(intent, "http://127.0.0.1:8073");
-		}			
-	});
-};
-
-//======================= iwc.util ==============================
-iwc.util = function() {
-};
-
-/**
- * Used to determine whether global or local messaging should be used.
- * Local messaging uses HTML5 messaging, global messaging uses yjs.
- */
-iwc.util.FLAGS = {
-	PUBLISH_LOCAL : "PUBLISH_LOCAL",
-	PUBLISH_GLOBAL : "PUBLISH_GLOBAL"
-};
-
-/**
- * Check intent for correctness.
- */
-iwc.util.validateIntent = function(intent) {
-	if (typeof intent.sender != "string") {
-		throw new Error("Intent object must possess property 'component' of type 'String'");
+	else {
+		console.log("IWC already defined.");
 	}
-	if (typeof intent.data != "string") {
-		throw new Error("Intent object must possess property 'data' of type 'String'");
-	}
-	if (typeof intent.dataType != "string") {
-		throw new Error("Intent object must possess property 'dataType' of type 'String'");
-	}
-	return true;
-};
+})(window);
