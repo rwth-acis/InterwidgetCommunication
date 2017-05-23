73c06c31757262ba387f833b16c96bc2691e078b
Jonas Könning <jonas.koenning@gmail.com>
2017-05-23 13:41:54 +0200

    Better documentation

--- TestWidgets/frontendComponent-TestWidget2/js/lib/iwc.js
+++ TestWidgets/frontendComponent-TestWidget2/js/lib/iwc.js
@@ -29,7 +29,43 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-// WIP re-implementation
+/**
+ * @class
+ * 
+ * iwc is a framework for local and remote interwidget by direct messaging (pub/sub approach might be added back soon). 
+ * The sending widget publishes a message which is then received at every other widget in the widget container. If the receiving widget
+ * matches a specified receiver, it processes the message (TODO: add broadcasting).
+ * If remote interwidget communication is configured the message is processed by <a href="http://y-js.org">Y-js</a> and all widgets of
+ * the same yjs room can be adressed.
+ * <p/>
+ * Messages exchanged between widgets follow a definitive structure called an <b>Intent</b>.
+ * Read about Intents in the <a href="http://developer.android.com/guide/topics/intents/intents-filters.html">Google Dev Guide</a>
+ * <p/>
+ * Intent messages (in the following called JSON intent objects) are of the following form:
+ * <br/>
+ * <b>{</b> <br/>
+ * 	<b>component</b>	:	(String) &lt; The name of the desired receiving widget. In the demo this is the widget's title &gt;, <br/>
+ * 	<b>sender</b>		:	(String) &lt; The name of the sending widget. In the demo this is the widget's title &gt;, <br/>
+ * 	<b>action</b>		:	(String) &lt; the action to be performed by receivers (e.g. "ACTION_INVOKE_SERVICE") &gt;, 
+ * 	<b>data</b> 		 	(String) &lt; data, currently just strings &gt;, <br/>
+ * 	<b>dataType</b>	:		(String) &lt; the data type in MIME notation (e.g. text/html) &gt;, <br/>
+ * 	<b>categories</b>	:	(Array) &lt; categories of widgets that shall process the intent (e.g. ["editor","proxy" ]) &gt;, <br/>
+ * 	<b>flags</b>		:	(Array) &lt; flags that control how the intent is processed (e.g. ["PUBLISH_GLOBAL"]) &gt;, <br/>
+ * 	<b>extras</b>		:	(Object) &lt; auxiliary data that need not to be specified. (e.g. key value pairs: {"examplekey":"examplevalue"}) &gt; <br/>
+ * <b>}</b> <br/>
+ * <p/>
+ * The framework is divided into three classes. 
+ * <ol>
+ *	<li><b>iwc.Proxy</b> is responsible for remote transport of interwidget communication intents</li>
+ * 	<li><b>{@link iwc.Client}</b> is responsible for client-side interaction with interwidget communication</li>
+ * 	<li><b>{@link iwc.util}</b> provides utility functions used by {@link iwc.Client} and iwc.Proxy</li>
+ * </ol>
+ * This part of the library only includes iwc.Client and iwc.util. The iwc.Proxy part is integrated with the ROLE Reference Implementation.
+ * 
+ * @author Christian Hocken (hocken@dbis.rwth-aachen.de)
+ * @author Dominik Renzel (renzel@dbis.rwth-aachen.de)
+ * @author Jonas Könning (koenning@dbis.rwth-aachen.de)
+ */
 (function (window) {
 	'use strict';
 	function define_IWC() {
@@ -46,6 +82,12 @@
 			this.extras = {};
 		};
 
+		/**
+		 * Provides messaging functionality.
+		 * @param {Array} categories - (currently not implemented) categories of widgets that shall process the intent (e.g. ["editor","proxy" ])
+		 * @param {String} origin - The origin (i.e. the url where your application script lives) is needed for messaging
+		 * @param {Y} y - A reference to yjs' Y object for global messaging
+		 */
 		IWC.Client = function (categories, origin, y) {
 
 			//console.log(y);
@@ -81,7 +123,8 @@
 
 		/**
 		 * Connect widget to messaging. This sets up the callback function and creates
-		 * an event listener for HTML5 messaging.
+		 * an event listener for HTML5 messaging and a yjs observer for global messaging.
+		 * If yjs is not available only local messaging is set up.
 		 * @param {function} callback - The callback function used for receiving messages.
 		 */
 		IWC.Client.prototype.connect = function (callback) {
@@ -98,7 +141,8 @@
 
 		/**
 		 * Disconnect the widget from messaging. This removes the event listener
-		 * and the callback.
+		 * and the callback for both local and global messaging. If yjs is not available,
+		 * only local messaging will be available.
 		 */
 		IWC.Client.prototype.disconnect = function () {
 			window.removeEventListener('message', receiveMessage, false);
@@ -140,16 +184,15 @@
 		};
 
 		/**
-		 * Unpack events and pre process them
+		 * Unpack events and pre process them. This unwraps HTML5 messages and manages the yjs map
+		 * used for global messaging.
 		 * @param {Event} event - The event that activated the callback
 		 */
 		var receiveMessage = function (event) {
-			//console.log(event);
 			// Local messaging events
 			if (event.type === "message") {
 				//Unpack message events
 				if (event instanceof MessageEvent) {
-					//console.log(event);
 					this._callback(event.data);
 				}
 			} else if (event.type === "add" || event.type == "update") {
